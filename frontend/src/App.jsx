87286640716738144f6bd710d5aee97a8ec7be8f
Logan White <135063791+LWhite49@users.noreply.github.com>
2025-03-09 19:53:39 -0400

    Made Infrastructure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+function App() {
+	// Define queries and mutations
+
+	const url = "http://127.0.0.1:8000/api/";
+	return <></>;
+}
+
+export default App;
